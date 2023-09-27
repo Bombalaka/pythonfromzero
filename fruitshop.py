0283ff07eec74c59f6fba8eb179a64053661b59b
Yotaka <124471935+Bombalaka@users.noreply.github.com>
2023-09-27 11:42:26 +0200

    Update fruitshop.py
    test if i can change anything in my code

--- fruitshop.py
+++ fruitshop.py
@@ -1,6 +1,8 @@
 fruit = {'apple' : 150,
-              'orange' : 300,
-               'durian' : 450}
+         'orange' : 300,
+         'durian' : 450,
+         'watermelon : 500'
+        }
 
 while True:
         product = input("Please choose the product: apple, durian, orange: ")
